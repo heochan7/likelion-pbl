787204f89ae000fba6710ab2502e08bb18075f60
Heochan1125 <110871957+heochan7@users.noreply.github.com>
2026-04-06 10:42:10 +0900

    미션 01 제출

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
-public class Main{
-    public static void main(String[] args){
-        System.out.println("Hello, LikeLion!");
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> members = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int n;
+        do{
+            System.out.print("아기사자 수를 입력하시오 : ");
+            n = sc.nextInt();
+        }while(n < 5);
+
+        for(int i=0; i<n; i++){
+            members.add(sc.nextLine());
+        }
+
+        for(int i=0; i<n; i++){
+            System.out.println(members.get(i));
+        }
     }
 }
