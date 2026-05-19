136e7c5de977a15052338cc969f42282d34cc9ac
Heochan1125 <110871957+heochan7@users.noreply.github.com>
2026-05-19 19:48:13 +0900

    미션 5 제출

--- src/package2/MemoryMemberRepository.java
+++ src/package2/MemoryMemberRepository.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class MemoryMemberRepository implements MemberRepository {
 
-    private ArrayList<Member> members = new ArrayList<>() ;
+    private final ArrayList<Member> members = new ArrayList<>() ;
 
     @Override
     public void save(Member member) {
